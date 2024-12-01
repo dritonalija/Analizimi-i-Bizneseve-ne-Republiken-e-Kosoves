99c002e6988bc190a5ed0e88d3d7e296c8194bb7
Driton Alija <driton.alija@student.uni-pr.edu>
2024-12-01 10:51:48 +0100

    improvements from first version

--- README.md
+++ README.md
@@ -1,6 +1,41 @@
 # Analizimi i Bizneseve në Republikën e Kosovës
 Studimi master 2024/2025. Lënda "Paraqitja dhe vizualizimi i të dhënave". Projekt grupor "Analizimi i Bizneseve në Republikën e Kosovës".
 
+<table>
+  <tr>
+   <td>
+     <img src="assets/uni-logo.png" alt="University Logo" width="200" >
+    </td>
+    <td>
+      <h2>UNIVERSITETI I PRISHTINËS “HASAN PRISHTINA”</h2>
+      <p><strong>Fakulteti i Inxhinierisë Elektrike dhe Kompjuterike</p>
+      <p><strong>Departamenti: </strong> Inxhinieri Kompjuterike</p>
+      <p><strong>Programi: </strong>Inxhinieri Kompjuterike dhe Softuerike</p>
+      <p><strong>Kodra e Diellit, p.n. - 10000 Prishtinë, Kosova</string>
+    </td>
+   
+  </tr>
+</table>
+  
+
+---
+
+## Detajet e lëndës
+- **Përgatitja dhe vizualizimi i të dhënave**
+- **Dr. Sc. Mërgim H. HOTI**
+- **Asst. Dr. Sc. Mërgim H. HOTI**
+- **Niveli:** Master
+- **Viti akademik:** 2024/2025
+
+---
+
+
+## Grupi punues
+  - Driton Alija
+  - Muhamet Zahiri
+  - Jusuf Maksuti
+  
+
 Të dhënat zyrtare të ARBK-së
 **Lidhja** 
 - [ ] Faqja kryesore: `https://arbk.rks-gov.net`
@@ -13,11 +48,23 @@ Të dhënat e marra nga faqja `Open Businesses Kosova`
 ## Përmbajtja
 
 - [Analizimi i Bizneseve në Republikën e Kosovës](#analizimi-i-bizneseve-në-republikën-e-kosovës)
+  - [Detajet e lëndës](#detajet-e-lëndës)
+  - [Grupi punues](#grupi-punues)
   - [Përmbajtja](#përmbajtja)
   - [Rreth Projektit](#rreth-projektit)
   - [Struktura e Projektit](#struktura-e-projektit)
-  - [Dokumentimi](#dokumentimi)
+- [Faza e parë - Para-Porcesimi i te dhenave](#faza-e-parë---para-porcesimi-i-te-dhenave)
   - [Kërkesat](#kërkesat)
+  - [Struktura e te dhenave](#struktura-e-te-dhenave)
+  - [Përgatitja e të Dhënave](#përgatitja-e-të-dhënave)
+    - [Funksionet Kryesore:](#funksionet-kryesore)
+    - [Standardizimi dhe Trajtimi i Vlerave që Mungojnë](#standardizimi-dhe-trajtimi-i-vlerave-që-mungojnë)
+    - [Transformimi, Agregimi dhe Reduktimi i Dimensionit](#transformimi-agregimi-dhe-reduktimi-i-dimensionit)
+  - [Mostrimi](#mostrimi)
+  - [Statistika](#statistika)
+  - [Datatype info](#datatype-info)
+  - [Korrelacioni](#korrelacioni)
+  - [Kërkesat](#kërkesat-1)
   - [Instalimi](#instalimi)
 
 ## Rreth Projektit
@@ -46,11 +93,94 @@ Këto statistika do të ndihmojnë në kuptimin më të mirë të strukturës dh
 - `README.md`: Skedari kryesor i përshkrimit të projektit.
 - `requirements.txt`: Kërkesat për bibliotekat e nevojshme Python.
 
-## Dokumentimi
+# Faza e parë - Para-Porcesimi i te dhenave
+## Kërkesat
 
-Këtu mund të gjeni dokumentimin e fazave te projektit. 
+Para-procesimi për përgatitjen e të dhënave për analizë.
 
-[Faza e pare](docs/pergatitja_e_te_dhenave.md)
+Mbledhja e të dhënave, definimi i tipeve të dhënave, kualiteti i të
+dhënave.
+
+Integrimi, agregimi, mostrimi, pastrimi, identifikimi dhe strategjia e
+trajtimit për vlerat e zbrazëta.
+
+Reduktimi i dimensionit, zgjedhja e nën bashkësisë së vetive, krijimi
+i vetive, diskretizimi dhe binarizimi, transformimi.
+
+## Struktura e te dhenave
+
+Datat jane marre nga https://biznesetehapura.com/info per 10 vitet e fundit. Keto data jane bashkuar sebashku duke perdorur scripten merge_data.py
+
+Struktura e te dhenave ne csv
+
+| Emri i biznesit            | Statusi | Numri fiskal | Tipi i biznesit     | Kapitali | Numri i punëtorëve | Pronarë       | Gjinia e pronarit | Personat e autorizuar | Data e regjistrimit | Linku në ARBK                                                                 | Numri i regjistrimit | Komuna | Aktivitetet         |
+|-----------------------------|---------|--------------|---------------------|----------|--------------------|---------------|-------------------|-----------------------|----------------------|--------------------------------------------------------------------------------|-----------------------|---------|----------------------|
+| John Doe B.I.        | Aktiv   | 600917621    | Biznes individual  | 0        | 1                  | John Doe  | Mashkull          | John                 | 2013-01-01  | (http://arbk.rks-gov.net/page.aspx?id=1,38,103658)                        | 70920638              | Lipjan  | Aktivitete juridike |
+
+
+Struktura pas procesimit
+
+| Emri i biznesit | Statusi   | Tipi i biznesit         | Data e regjistrimit | Data e mbylljes | Komuna     | Kapitali | Numri i punëtorëve | Pronarë Mashkull | Pronarë Femër | Aktivitetet Encoded |
+|-----------------|-----------|-------------------------|----------------------|-----------------|------------|----------|--------------------|------------------|---------------|----------------------|
+| John Doe B.I.   | 1    | 1       | 2013-01-01          | 2023-01-01      | 1           | 0        | 1                  | 1                | 0             | [6]                 |
+
+
+## Përgatitja e të Dhënave
+
+Për të përgatitur të dhënat është përdorur skripta `data_preparation.py`.
+
+Janë përdorur librari të ndryshme të Python-it si dhe ChatGPT për këshillime.
+
+### Funksionet Kryesore:
+
+- **load_data(file_path)**: Ngarkon të dhënat nga një skedar CSV.
+- **datatype_format(df)**: Formaton kolonat në tipe të sakta (data dhe numra).
+- **normalize_status(df)**: Normalizon kolonën `Statusi` dhe nxjerr datën për `Data e mbylljes`.
+- **update_gender_column(df)**: Përcakton gjininë e pronarëve bazuar në emrat.
+- **count_genders(df)**: Numëron pronarët sipas gjinisë në kolonat `Pronarë Mashkull` dhe `Pronarë Femër`.
+- **encode_aktivitetet(df)**: Kodon kolonën `Aktivitetet` dhe ruan hartimin.
+- **remove_duplicates(df)**: Heq rreshtat e dyfishtë bazuar në `Uid`.
+- **handle_missing_values(df)**: Trajton vlerat që mungojnë.
+
+### Standardizimi dhe Trajtimi i Vlerave që Mungojnë
+
+Për të trajtuar vlerat që mungojnë janë përdorur disa strategji.
+
+P.sh., për mungesën e gjinisë së pronarit, është përdorur libraria `gender-guesser` e Python-it për të përcaktuar gjininë nga emri (gjinia e pronarit nuk është në faqen zyrtare të ARBK).
+
+Për mungesën e komunës, është krijuar një `crawler` (`arbk_crawler.py`) i cili, duke përdorur framework-un Playwright, shkon në faqen e ARBK, kërkon me emër biznesi dhe pasqyron ato të dhëna nga përgjigja në CSV. Kjo për shkak se ARBK ka mekanizma që mbrojnë fuqishëm marrjen e të dhënave nga crawler-at dhe gjithashtu përdor Google Recaptcha.
+
+`Data e mbylljes` është përcaktuar nga `Statusi`, p.sh., `pasiv-2022/06/01`.
+
+Të gjitha vlerat e bashkësive, sikurse `Statusi`, `Komunat`, `Tipi i Biznesit`, janë pasqyruar në një skedar `JSONL` dhe në të dhënat e procesuara vendosen vlera numerike për performancë më të mirë.
+
+`Aktivitetet` i kodojmë si një listë.
+
+### Transformimi, Agregimi dhe Reduktimi i Dimensionit
+
+Duke u bazuar në strukturën aktuale, kemi krijuar disa kolona të reja, p.sh., `Pronarë Femër`, `Pronarë Mashkull` (për të përcaktuar numrin e pronarëve në një biznes).
+
+Kemi larguar kolona të panevojshme, sikurse `Numri Fiskal`, `Numri i Regjistrimit`, emrat e pronarëve, etj.
+
+Në `data_transformation.py`, kemi gjithashtu shembuj të binarizimit të kolonave, sikurse `Kapitali`, `Numri i punëtorëve` dhe `Business Age`.
+
+## Mostrimi
+
+Në skedarin `data_samples.py` janë bërë disa forma të thjeshta të zgjedhjes së mostrës.
+
+
+## Statistika
+
+Në skedarin `data_stats.py` janë bërë disa statistika me datat e procesuara.
+
+## Datatype info
+
+Në skedarin `dataset_info.py` janë gjinden përshkruhen të gjitha vetitë e dataset.
+
+## Korrelacioni
+
+Në skedarin `correlation_matrix.py` jipet matrixa e korrelacionit.
+
 
 
 ## Kërkesat
--- src/data_preparation.py
+++ src/data_preparation.py
@@ -20,7 +20,9 @@ def load_data(file_path):
 # Funksioni për të konvertuar kolonat në formatin e duhur të të dhënave
 def datatype_format(df):
     """Kthen kolonat në formatin e duhur, duke definuar tipin e të dhënave."""
-    df["Data e regjistrimit"] = pd.to_datetime(df["Data e regjistrimit"], errors="coerce")
+    df["Data e regjistrimit"] = pd.to_datetime(
+        df["Data e regjistrimit"], errors="coerce"
+    )
     df["Kapitali"] = pd.to_numeric(df["Kapitali"], errors="coerce")
     df["Numri i punëtorëve"] = pd.to_numeric(df["Numri i punëtorëve"], errors="coerce")
     return df
@@ -29,8 +31,14 @@ def datatype_format(df):
 # Funksioni për të normalizuar 'Statusi' dhe për të nxjerrë datën në 'Data e mbylljes'
 def normalize_status(df):
     """Normalizon vlerat specifike në kolonën 'Statusi' dhe nxjerr datat për kolonën 'Data e mbylljes'."""
-    df['Data e mbylljes'] = pd.to_datetime(df['Statusi'].str.extract(r'(\d{2}/\d{2}/\d{4})')[0], format='%d/%m/%Y', errors='coerce')
-    df['Statusi'] = df['Statusi'].replace({r"(?i)pasiv.*": "Pasiv", r"(?i)anuluar nga sistemi": "Shuar"}, regex=True)
+    df["Data e mbylljes"] = pd.to_datetime(
+        df["Statusi"].str.extract(r"(\d{2}/\d{2}/\d{4})")[0],
+        format="%d/%m/%Y",
+        errors="coerce",
+    )
+    df["Statusi"] = df["Statusi"].replace(
+        {r"(?i)pasiv.*": "Pasiv", r"(?i)anuluar nga sistemi": "Shuar"}, regex=True
+    )
     return df
 
 
@@ -38,30 +46,52 @@ def normalize_status(df):
 def detect_gender_gender_guesser(name):
     """Përcakton gjininë bazuar në emrin e parë."""
     guess = d.get_gender(HumanName(name).first)
-    return "Mashkull" if guess in ["male", "mostly_male"] else "Femër" if guess in ["female", "mostly_female"] else "Panjohur"
+    return (
+        "Mashkull"
+        if guess in ["male", "mostly_male"]
+        else "Femër" if guess in ["female", "mostly_female"] else "Panjohur"
+    )
 
 
 # Funksioni për të përditësuar kolonën 'Gjinia e pronarit' duke deduktuar gjininë nga emrat
 def update_gender_column(df):
     """Përditëson kolonën 'Gjinia e pronarit' për të ruajtur gjininë e deduktuar nga emrat."""
     df["Gjinia e pronarit"] = df.apply(
-        lambda row: ", ".join(detect_gender_gender_guesser(name.strip()) for name in str(row["Pronarë"]).split(","))
-        if "Panjohur" in str(row["Gjinia e pronarit"]) else row["Gjinia e pronarit"], axis=1)
+        lambda row: (
+            ", ".join(
+                detect_gender_gender_guesser(name.strip())
+                for name in str(row["Pronarë"]).split(",")
+            )
+            if "Panjohur" in str(row["Gjinia e pronarit"])
+            else row["Gjinia e pronarit"]
+        ),
+        axis=1,
+    )
     return df
 
 
 # Funksioni për të numëruar përsëritjet e gjinive në kolonën 'Gjinia e pronarit'
 def count_genders(df):
     """Krijon kolonat 'Pronarë Mashkull' dhe 'Pronarë Femër' për të numëruar përsëritjet në 'Gjinia e pronarit'."""
-    df["Pronarë Mashkull"] = df["Gjinia e pronarit"].apply(lambda x: [g.strip() for g in str(x).split(",")].count("Mashkull"))
-    df["Pronarë Femër"] = df["Gjinia e pronarit"].apply(lambda x: [g.strip() for g in str(x).split(",")].count("Femër"))
+    df["Pronarë Mashkull"] = df["Gjinia e pronarit"].apply(
+        lambda x: [g.strip() for g in str(x).split(",")].count("Mashkull")
+    )
+    df["Pronarë Femër"] = df["Gjinia e pronarit"].apply(
+        lambda x: [g.strip() for g in str(x).split(",")].count("Femër")
+    )
     return df
 
 
 # Funksioni për të nxjerrë 'Uid' nga fundi i një lidhje për regjistrin
 def extract_registry_number(df):
     """Krijon kolonën 'Uid' duke marrë pjesën e fundit të lidhjes dhe e kthen atë në numër të plotë."""
-    df["Uid"] = df["Linku në ARBK"].str.split(",").str[-1].apply(lambda x: int(x) if pd.notnull(x) and x.isdigit() else None).astype("Int64")
+    df["Uid"] = (
+        df["Linku në ARBK"]
+        .str.split(",")
+        .str[-1]
+        .apply(lambda x: int(x) if pd.notnull(x) and x.isdigit() else None)
+        .astype("Int64")
+    )
     return df
 
 
@@ -69,11 +99,23 @@ def extract_registry_number(df):
 def fill_business_type(df):
     """Mbush 'Tipi i biznesit' bazuar në emrin e biznesit."""
     type_map = {
-        "B.I.": "Biznes individual", "SH.P.K.": "Shoqëri me përgjegjësi të kufizuara", "L.L.C.": "Shoqëri me përgjegjësi të kufizuara",
-        "K.B.": "Kooperativa Bujqësore", "N.SH.": "Ndërmarrje shoqërore", "O.K.": "Ortakëri e kufizuar",
-        "O.P.": "Ortakëri e përgjithshme", "SH.A.": "Shoqëri aksionare", "Z.P.K.": "Zyra e Përfaqësisë në Kosovë"
+        "B.I.": "Biznes individual",
+        "SH.P.K.": "Shoqëri me përgjegjësi të kufizuara",
+        "L.L.C.": "Shoqëri me përgjegjësi të kufizuara",
+        "K.B.": "Kooperativa Bujqësore",
+        "N.SH.": "Ndërmarrje shoqërore",
+        "O.K.": "Ortakëri e kufizuar",
+        "O.P.": "Ortakëri e përgjithshme",
+        "SH.A.": "Shoqëri aksionare",
+        "Z.P.K.": "Zyra e Përfaqësisë në Kosovë",
     }
-    df["Tipi i biznesit"] = df.apply(lambda row: next((v for k, v in type_map.items() if row["Emri i biznesit"].endswith(k)), row["Tipi i biznesit"]), axis=1)
+    df["Tipi i biznesit"] = df.apply(
+        lambda row: next(
+            (v for k, v in type_map.items() if row["Emri i biznesit"].endswith(k)),
+            row["Tipi i biznesit"],
+        ),
+        axis=1,
+    )
     return df
 
 
@@ -139,7 +181,12 @@ def encode_columns(df, columns, output_dir="../data/processed"):
         jsonl_file_path = os.path.join(output_dir, f"{column}_mapping.jsonl")
 
         # Merr hartimet unike për kolonën aktuale
-        unique_mappings = {int(label): class_name for label, class_name in zip(label_encoder.transform(label_encoder.classes_), label_encoder.classes_)}
+        unique_mappings = {
+            int(label): class_name
+            for label, class_name in zip(
+                label_encoder.transform(label_encoder.classes_), label_encoder.classes_
+            )
+        }
 
         # Shkruaj të gjitha hartimet unike në skedarin JSONL
         with open(jsonl_file_path, "w") as file:
@@ -150,20 +197,36 @@ def encode_columns(df, columns, output_dir="../data/processed"):
 
     return df
 
+
 def preprocess_data(file_path, output_path):
     """Përpunimi i të dhënave për finalizim."""
-    df = (load_data(file_path)
-          .pipe(extract_registry_number)
-          .drop_duplicates("Uid")
-          .pipe(datatype_format)
-          .pipe(normalize_status)
-          .pipe(update_gender_column)
-          .pipe(count_genders)
-          .pipe(fill_business_type)
-          .fillna({"Numri i punëtorëve": 0, "Kapitali": 0})
-          .pipe(encode_aktivitetet)
-          .pipe(encode_columns, ["Statusi", "Tipi i biznesit", "Komuna"])
-          [["Emri i biznesit", "Statusi", "Tipi i biznesit", "Data e regjistrimit", "Data e mbylljes", "Komuna", "Kapitali", "Numri i punëtorëve", "Pronarë Mashkull", "Pronarë Femër", "Aktivitetet Encoded"]])
+    df = (
+        load_data(file_path)
+        .pipe(extract_registry_number)
+        .drop_duplicates("Uid")
+        .pipe(datatype_format)
+        .pipe(normalize_status)
+        .pipe(update_gender_column)
+        .pipe(count_genders)
+        .pipe(fill_business_type)
+        .fillna({"Numri i punëtorëve": 0, "Kapitali": 0})
+        .pipe(encode_aktivitetet)
+        .pipe(encode_columns, ["Statusi", "Tipi i biznesit", "Komuna"])[
+            [
+                "Emri i biznesit",
+                "Statusi",
+                "Tipi i biznesit",
+                "Data e regjistrimit",
+                "Data e mbylljes",
+                "Komuna",
+                "Kapitali",
+                "Numri i punëtorëve",
+                "Pronarë Mashkull",
+                "Pronarë Femër",
+                "Aktivitetet Encoded",
+            ]
+        ]
+    )
 
     os.makedirs(os.path.dirname(output_path), exist_ok=True)
     df.to_csv(output_path, index=False)
